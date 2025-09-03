0511aa5872714b864c07fdfcb2febe363ac7f4e0
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-09-03 16:00:44 -0300

    Atualização da classe abstrata DAO para implementar AutoCloseable e manejar automaticamente a abertura e o fechamento da conexão

--- src/main/java/com/dao/DAO.java
+++ src/main/java/com/dao/DAO.java
@@ -1,11 +1,21 @@
 package com.dao;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import com.database.ConnectionFactory;
 
 // Classe abstrata para facilitar o gerenciamento de conexões dos DAOs
-public abstract class DAO {
+public abstract class DAO implements AutoCloseable {
     protected static final ConnectionFactory connF = new ConnectionFactory();
     protected Connection conn;
+
+    protected DAO() throws SQLException {
+        conn = connF.getConnection();
+    }
+
+    @Override
+    public void close() throws SQLException {
+        connF.closeConnection(conn);
+    }
 }
