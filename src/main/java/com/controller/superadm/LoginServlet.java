8e80a74262e95d697bcec2385dcad71ef3d88e9f
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-09-08 16:41:45 -0300

    Reorganização do pacote controller para separar o servlets por assunto

--- src/main/java/com/controller/superadm/LoginServlet.java
+++ src/main/java/com/controller/superadm/LoginServlet.java
@@ -1,4 +1,4 @@
-package com.controller;
+package com.controller.superadm;
 
 import com.dao.LoginDAO;
 import com.dto.LoginDTO;
--- src/main/java/com/controller/superadm/LogoutServlet.java
+++ src/main/java/com/controller/superadm/LogoutServlet.java
@@ -1,4 +1,4 @@
-package com.controller;
+package com.controller.superadm;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
