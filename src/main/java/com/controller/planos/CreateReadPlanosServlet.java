c2ed9a9a4c134af804b8cd05f686b9c118f3c8d8
ryancursinom <rcursinomoraes@gmail.com>
2025-09-09 22:00:29 -0300

    Primeira versão dos Servlets da tabela Planos

--- src/main/java/com/controller/planos/CreateReadPlanosServlet.java
+++ src/main/java/com/controller/planos/CreateReadPlanosServlet.java
@@ -1,7 +1,10 @@
-package com.controller;
+package com.controller.planos;
 
 import com.dao.PlanosDAO;
+import com.dao.SuperAdmDAO;
 import com.dto.PlanosDTO;
+import com.dto.SuperAdmDTO;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,21 +13,24 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-@WebServlet ("/area-restrita/planos/create-plano")
+@WebServlet ("/area-restrita/create-plano")
 public class CreateReadPlanosServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //Pegando atributos
+        //Dados da Request
         String nome = req.getParameter("nome");
         Double valor = Double.parseDouble(req.getParameter("valor"));
         String descricao = req.getParameter("descricao");
 
-        //Instanciando objetos
+        //Instanciando DTO
         PlanosDTO planosDTO = new PlanosDTO(null,nome, valor, descricao);
-        try (PlanosDAO planosDAO = new PlanosDAO()){
-            //Chamando metodo de cadastro
+
+        try (PlanosDAO planosDAO = new PlanosDAO()){ //DAO instanciado com try-with-resources
+            //Cadastra o plano
             planosDAO.cadastrar(planosDTO);
 
             resp.sendRedirect(req.getContextPath() + "/area-restrita/index.jsp");
@@ -39,6 +45,45 @@ public class CreateReadPlanosServlet extends HttpServlet {
         } catch(Throwable t){
             System.err.println("Erro não identificado");
             resp.sendRedirect(req.getContextPath()+"/erro.html");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        // Dados da resposta
+        List<PlanosDTO> planosDTOS;
+        boolean erro = true;
+
+        try (PlanosDAO dao = new PlanosDAO()) {
+
+            // Recupera os usuários do banco e registra na request
+            planosDTOS = dao.listarPlanos();
+            req.setAttribute("planos", planosDTOS);
+
+            // setta erro como false
+            erro = false;
+
+        } catch (SQLException e) {
+            // Se houver alguma exceção, registra no terminal
+            System.err.println("Erro ao executar operação no banco:");
+            e.printStackTrace(System.err);
+
+        } catch (ClassNotFoundException e) {
+            System.err.println("Falha ao carregar o driver postgresql:");
+            e.printStackTrace(System.err);
+
+        } catch (Throwable e) {
+            System.err.println("Erro inesperado:");
+            e.printStackTrace(System.err);
+
+        }
+
+        // Redireciona a request par a página jsp
+        if (erro) {
+            resp.sendRedirect(req.getContextPath() + "/erro.html");
+        } else {
+            RequestDispatcher rd = req.getRequestDispatcher("planos/index.jsp");
+            rd.forward(req, resp);
         }
     }
 }
