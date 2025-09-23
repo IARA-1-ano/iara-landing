e554a3cbf0c902296b4f74018639b0b672814137
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-09-23 14:46:52 -0300

    Implementação das mudanças em ExcecaoDePagina.java

--- src/main/java/com/exception/ExcecaoDePagina.java
+++ src/main/java/com/exception/ExcecaoDePagina.java
@@ -1,47 +1,43 @@
 package com.exception;
 
 public class ExcecaoDePagina extends RuntimeException {
-  private final String metodoCausador;
-
-  public ExcecaoDePagina(String message, String metodoCausador) {
+  public ExcecaoDePagina(String message) {
     super(message);
-    this.metodoCausador = metodoCausador;
   }
 
   // Métodos de fábrica para padronização da mensagem
-  public static ExcecaoDePagina campoUnicoDuplicado(String entidade, String campo, boolean subsFem, String metodoCausador) {
+  public static ExcecaoDePagina campoUnicoDuplicado(String entidade, String campo, boolean subsFem) {
     String msg = "Já existe %s %s %s com esse %s".formatted(subsFem ? "uma" : "um", entidade, subsFem ? "registrada" : "registrado", campo);
-    return new ExcecaoDePagina(msg, metodoCausador);
+    return new ExcecaoDePagina(msg);
   }
 
-  public static ExcecaoDePagina cnpjDuplicado(String metodoCausador) {
-    return campoUnicoDuplicado("fábrica", "cnpj", true, metodoCausador);
+  public static ExcecaoDePagina cnpjDuplicado() {
+    return campoUnicoDuplicado("fábrica", "cnpj", true);
   }
 
-  public static ExcecaoDePagina cpfDuplicado(String entidade, String metodoCausador) {
-    return campoUnicoDuplicado(entidade, "cpf", false, metodoCausador);
+  public static ExcecaoDePagina cpfDuplicado(String entidade) {
+    return campoUnicoDuplicado(entidade, "cpf", false);
   }
 
-  public static ExcecaoDePagina emailDuplicado(String entidade, String metodoCausador) {
-    return campoUnicoDuplicado(entidade, "email", false, metodoCausador);
+  public static ExcecaoDePagina emailDuplicado(String entidade) {
+    return campoUnicoDuplicado(entidade, "email", false);
   }
 
-  public static ExcecaoDePagina nomeDuplicado(String metodoCausador) {
-    return campoUnicoDuplicado("plano", "nome", false, metodoCausador);
+  public static ExcecaoDePagina nomeDuplicado() {
+    return campoUnicoDuplicado("plano", "nome", false);
   }
 
-  public static ExcecaoDePagina senhaCurta(int tamanhoMinimo, String metodoCausador) {
+  public static ExcecaoDePagina senhaCurta(int tamanhoMinimo) {
     String msg = "A senha deve ter pelo menos %d dígitos".formatted(tamanhoMinimo);
-    return new ExcecaoDePagina(msg, metodoCausador);
+    return new ExcecaoDePagina(msg);
   }
 
-  public static ExcecaoDePagina campoNecessarioFaltante(String campo, String metodoCausador) {
+  public static ExcecaoDePagina campoNecessarioFaltante(String campo) {
     String msg = "Preencha o campo %s antes de enviar".formatted(campo);
-    return new ExcecaoDePagina(msg, metodoCausador);
+    return new ExcecaoDePagina(msg);
   }
 
-  // Getters
-  public String getMetodoCausador() {
-    return metodoCausador;
+  public static ExcecaoDePagina falhaAutenticacao() {
+    return new ExcecaoDePagina("Autenticação falhou. Verifique suas credenciais.");
   }
 }
