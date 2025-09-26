303456671ebb117fb14b5fb6305e07b2fdc998d2
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-09-26 14:31:09 -0300

    Criação de classe utilitária com formatador de data estático

---
+++ src/main/java/com/utils/DateUtils.java
@@ -0,0 +1,7 @@
+package com.utils;
+
+import java.time.format.DateTimeFormatter;
+
+public class DateUtils {
+  public static final DateTimeFormatter isoDmy = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+}
