63b9e02c84e119a51249accb1320dce112b74707
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-10-18 09:50:52 -0300

    Criação da função capitalize() em StringUtils.java

---
+++ src/main/java/com/utils/StringUtils.java
@@ -0,0 +1,11 @@
+package com.utils;
+
+public class StringUtils {
+  public static String capitalize(String s) {
+    if (s.length() < 2) {
+      return s.toUpperCase();
+    }
+
+    return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
+  }
+}
